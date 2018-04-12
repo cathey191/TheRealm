f73c97e7d28cf779c73c746538845d722d07e19c
cathey191 <cathey191@hotmail.com>
2018-04-13 11:49:36 +1200

    PopUp Started

--- js/script.js
+++ js/script.js
@@ -13,17 +13,55 @@ $(document).ready(function(){
 	 }
 
 	 var date = new Date();
+	 var day = date.getDay();
 	 var hour = date.getHours();
 
 	 if (hour < 16){
-		 $(".brunch").show()
+		 $(".brunch").show();
 	 } else {
 		  $(".brunch").hide();
 	 }
 
-	 if (hour > 2){
-		 $(".pizza, .dinner, .desserts").show()
+	 if (hour > 16){
+		 $(".pizza, .dinner, .desserts").show();
 	 } else {
 		  $(".pizza, .dinner, .desserts").hide();
 	 }
+
+	 // if (day == 4 && hour >= 16){
+	 if (hour >= 2){
+		 $(".pizzaDeal").show();
+		 $(".backgroundPop").removeClass("display");
+	 } else {
+		 $(".pizzaDeal").hide();
+	 }
+	 if (day == 1 || 3 && hour >= 16){
+		 $(".twoMains").show();
+	 } else {
+		 $(".twoMains").hide();
+	 }
+
+
+	 // $(function(){
+		//  var overlay = $('<div id="overlay"></div>');
+		//  overlay.show();
+		//  overlay.appendTo(document.body);
+		//  $('.popup').show();
+		//  $('.close').click(function(){
+		// 	 $('.popup').hide();
+		// 	 overlay.appendTo(document.body).remove();
+		// 	 return false;
+		//  });
+
+
+
+
+		//  $('.x').click(function(){
+		// 	 $('.popup').hide();
+		// 	 overlay.appendTo(document.body).remove();
+		// 	 return false;
+		//  });
+	 // });
+
+
 });
