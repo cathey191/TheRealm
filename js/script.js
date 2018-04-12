deed352d2a5f9970b6ff7e90f00e9e9ac6bb00f2
Catherine <cathey191@hotmail.com>
2018-04-12 20:38:46 +1200

    Favourite menu changing in the mornings

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 		$(this).find('.originalText').hide();
 		$(this).append("<span class='removeButton'>Remove<span>");
 
-		// function WITHOUT ()'s, then timeout, then values to give the function
 		setTimeout(changeback, 3000, $(this));
 	});
 
@@ -12,53 +11,19 @@ $(document).ready(function(){
 		test.find('.removeButton').remove();
 	 	test.find('.originalText').show();
 	 }
+
+	 var date = new Date();
+	 var hour = date.getHours();
+
+	 if (hour < 16){
+		 $(".brunch").show()
+	 } else {
+		  $(".brunch").hide();
+	 }
+
+	 if (hour > 16){
+		 $(".pizza").show()
+	 } else {
+		  $(".pizza").hide();
+	 }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// EXTRA CREDIT....
-// replace with this:
-
-
-// $(document).on('click', '.add', function(){
-// 	var timeout = 5;
-// 	$(this).toggleClass('add remove');
-// 	$(this).find('.originalText').hide();
-// 	$(this).append("<span class='removeButton'>Remove  "+timeout+"<span>");
-//
-// 	for (var i=timeout;i>0;i--){
-// 		setTimeout(fancyChangeback, i*1000, [$(this), timeout-i]);
-// 	};
-// });
-//
-//
-// function fancyChangeback(input){
-//  var test = input[0];
-//  var i = input[1];
-//
-//  if (i != 0){
-//     test.find(".removeButton").text("Remove "+i);
-// } else {
-//     test.find('.removeButton').remove();
-//     test.find('.originalText').show();
-// };
-// };
-// });
