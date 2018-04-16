2af0411a44e34fd1af6aa00c65bed1d93b9b61d1
Catherine <cathey191@hotmail.com>
2018-04-16 20:57:12 +1200

    Tidying up js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,46 @@
 $(document).ready(function(){
-
+	// Add to order
 	$(document).on('click', '.add', function(){
 		$(this).find('.originalText').hide();
 		$(this).append('<span class="removeButton">Remove<span>');
-
-		setTimeout(changeback, 3000, $(this));
+		setTimeout(function (test){
+ 			test.find('.removeButton').remove();
+ 	 		test.find('.originalText').show();
+ 	 	}, 3000, $(this));
 	});
 
-	 function changeback(test){
-		test.find('.removeButton').remove();
-	 	test.find('.originalText').show();
-	 }
-
+	// Pop Ups
+	$('.drinks, .kMenu, .brunch, .dinner, .desserts').click(function(){
+		$('.popDError').removeClass('display')
+	});
+	$('.cash').click(function(){
+		$('.atCont').removeClass('display')
+		$('.counter').removeClass('display')
+	});
+	$('.CC, .tab').click(function(){
+		$('.atCont').removeClass('display')
+		$('.onCard').removeClass('display')
+	});
+	// Crosses on pop ups
+	$('.close').click(function(){
+	 $('.popContainer, .popDError').addClass('display');
+ });
+ 
+	// variables
 	 var date = new Date();
 	 var day = date.getDay();
 	 var hour = date.getHours();
 
-	 if (hour < 16){
+	 // Change menu by days
+	 if (hour <= 16){
 		 $('.brunch').show();
+		 $('.pizza, .dinner, .desserts').hide();
 	 } else {
 		  $('.brunch').hide();
+			$('.pizza, .dinner, .desserts').show();
 	 }
-
-	 if (hour >= 16){
-		 $('.pizza, .dinner, .desserts').show();
-	 } else {
-		  $('.pizza, .dinner, .desserts').hide();
-	 }
-
-	 if (day == 4 && hour >= 16 || day == 1){
+   // day 5 to show in presentation on Friday
+	 if (day == 4 && hour >= 16 || day == 5){
 		 $('.pizzaDeal, .pizza').show();
 		 $('.popContainer').removeClass('display');
 	 } else {
@@ -39,21 +51,5 @@ $(document).ready(function(){
 	 } else {
 		 $('.twoMains').hide();
 	 }
-
-	 $('.drinks, .kMenu, .brunch, .dinner, .desserts').click(function(){
-		 $('.popDError').removeClass('display')
-	 });
-	 $('.cash').click(function(){
-		 $('.atCont').removeClass('display')
-		 $('.counter').removeClass('display')
-	 });
-	 $('.CC, .tab').click(function(){
-		 $('.atCont').removeClass('display')
-		 $('.onCard').removeClass('display')
-	 });
-
-	 $('.close').click(function(){
- 		$('.popContainer, .popDError').addClass('display');
- 	});
 
 });
